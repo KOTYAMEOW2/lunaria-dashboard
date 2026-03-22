de5e6091073febbcaed679b04dac5bb8641fea91
KOTYAMEOW2 <maksim.2005andreev1@gmail.com>
2026-03-22 14:28:32 +0300

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-window.LUNARIA_CONFIG = {
-  SUPABASE_URL: "https://hqggzsfcswtqgwejblxe.supabase.co",
-  SUPABASE_KEY: "sb_publishable_6AmJxlgJz9BN47fIagW5lg_zjxAguyd",
-  REDIRECT_TO: "https://lunaria-dashboard.pages.dev/",
-  STORAGE_KEYS: {
-    GUILD_CONFIG: "guild-config.json"
-  }
-};
